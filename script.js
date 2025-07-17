aab4d763ecb4c39f32c57bf64516d4d29d4da4c7
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-07-17 23:15:13 +0530

    update all files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000, // 1 second animation
+    easing: 'ease-in-out',
+    once: true, // Only animate once when scrolling down
+});
